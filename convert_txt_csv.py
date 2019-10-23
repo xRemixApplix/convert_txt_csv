b5019f1b7cf90dc6de90d666c96cccb7cb5ee40c
xRemixApplix <r3m1.appli@gmail.com>
2019-10-23 12:21:01 +0200

    Ajout de la conversion en fichier Excel + Choix de la conversion en début de script (entre csv ou excel)

--- convert_txt_csv.py
+++ convert_txt_csv.py
@@ -5,49 +5,68 @@ import pandas
 
 startTime = time.time()
 
+choix = int(input("Choix de conversion (0->CSV ou 1->EXCEL)\n"))
+
+SOURCE_ENCODING = "utf-16 LE"
+TARGET_ENCODING = "utf-8"
+
 # Recuperation de la liste des fichiers du dossier
 listFiles = os.listdir("aConvertir")
 for fileId in range(0, len(listFiles)):
     listFiles[fileId] = listFiles[fileId].split('.')[0]
 
-for fileName in listFiles:
-    try:
-        # Ouverture du fichier .txt a convertir
-        file = open("aConvertir/"+fileName+".txt", 'r')
-        assert fileName != "readme"
+if choix==0 or choix==1 :
+    for fileName in listFiles:
+        try:
+            # Ouverture du fichier .txt a convertir
+            file = open("aConvertir/"+fileName+".txt", 'r')
+            assert fileName != "readme" 
+            
+        except Exception:
+            pass 
         
-    except Exception:
-        pass 
-    
-    else:
-        # Lecture du fichier
-        new_text = file.readlines()
+        else:
+            if choix == 0 :
+                # Lecture du fichier
+                new_text = file.readlines()
 
-        # Creation d'une liste contenant tout les mots dans le fichier et initialisation variables
-        words = []
-        line_break = 0
-
-        # Ajout des mots du fichier dans la liste
-        for x in range(0, len(new_text)):
-            for word in new_text[x].split("\t"):
-                words.append(word + ',')
-
-        # Ecriture des mots dans le nouveau fichier .csv
-        f = open("result/"+fileName+'.csv','w')
-
-        for x in range(0, len(words)):
-            if (line_break == 4):
-                f.write('\n')
-                f.write(words[x])
+                # Creation d'une liste contenant tout les mots dans le fichier et initialisation variables
+                words = []
                 line_break = 0
-            elif line_break < 3:
-                f.write(words[x])
+
+                # Ajout des mots du fichier dans la liste
+                for x in range(0, len(new_text)):
+                    for word in new_text[x].split(";"):
+                        words.append(word + ',')
+
+                # Ecriture des mots dans le nouveau fichier .csv
+                f = open("result/"+fileName+".csv",'w')
+
+                for x in range(0, len(words)):
+                    if (line_break == 4):
+                        f.write('\n')
+                        f.write(words[x])
+                        line_break = 0 
+                    elif line_break < 3:
+                        f.write(words[x])
+                        
+                    line_break += 1
+                f.close()
+            elif choix == 1 :
+                fileUtf16 = "aConvertir/"+fileName+".txt"
+                fileUtf8 = "aConvertir/"+fileName+"_utf8.txt"
                 
-            line_break += 1
-        f.close()
-        
-df = pandas.read_csv('test.txt', delimiter=" ")
-df.to_excel('output.xlsx', 'Sheet1', index=False)
+                with open(fileUtf16, "r", encoding=SOURCE_ENCODING) as fich :
+                    content = fich.read()
+                with open(fileUtf8, "w", encoding=TARGET_ENCODING) as fich :
+                    fich.write(content)
+                            
+                df = pandas.read_csv("aConvertir/"+fileName+"_utf8.txt", sep=";")
+                df.to_excel("result/"+fileName+".xlsx", 'Sheet1', index=False)
+                
+                print("Conversion du fichier {} : OK".format(fileName+".txt"))
+else:
+    print("Choix de conversion incorrecte.")
 
 # Confirmation de fin d'execution du script
 print("########################")
